f983fca8299fdbc26d01a1434bda2ffc57a9edac
wh629 <wh629@nyu.edu>
2020-04-28 15:41:48 -0400

    changed .out location in sbatch

--- experiment_scripts/hyper_parameter_tuning.py
+++ experiment_scripts/hyper_parameter_tuning.py
@@ -10,7 +10,7 @@ from shared_settings import make_command
 
 def select_candidates(dataset):
     lr_candidates = [1e-5, 3e-5, 5e-5]
-    bs_candidates = [8]
+    bs_candidates = [8, 16, 32]
     max_epochs_candidates = [10, 20, 40]
     seed_range = 1e6
     lr = lr_candidates[random.randrange(0, len(lr_candidates), 1)]
